912836f562f15cba9fe8ba288e69817803aad77b
binyamin zaetz <bzaetz@outlook.com>
2023-06-15 20:06:34 -0400

    base java commit

---
+++ src/main/java/com/znc/CHBNB/ChbnbApplication.java
@@ -0,0 +1,13 @@
+package com.znc.CHBNB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChbnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/znc/chbnb/ChbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znc.chbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
